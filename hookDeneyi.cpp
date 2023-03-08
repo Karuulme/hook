86877f66d28704fb130bf701e26981baeda0f73a
Karuulme <64870233+Karuulme@users.noreply.github.com>
2023-03-08 23:36:56 +0300

    hook

--- hookDeneyi.cpp
+++ hookDeneyi.cpp
@@ -7,92 +7,114 @@
 using namespace std;
 
 #define FIRSTBYTECPY 5
-#define TRAMPOLINBYTE 40
+#define TRAMPOLINEBYTE 40
+#define SHORTJUMPBYTE 15
 typedef struct _INFORMATIONABOUTHOOKEDFUNCTIONS {
-    ULONG nameAddress;
     ULONG oriAddress;
     ULONG hookFuncAddress;
-    VOID* tramFuncAddress;
-    VOID* tramAddress;
-;
+    VOID* shortJumpAddress = NULL;
+    VOID* trambolineAddress  = NULL;
 }INFORMATIONABOUTHOOKEDFUNCTIONS, *PINFORMATIONABOUTHOOKEDFUNCTIONS;
+vector<pair<ULONG, INFORMATIONABOUTHOOKEDFUNCTIONS>> _HookedFunction;
 
-vector<pair<ULONG, INFORMATIONABOUTHOOKEDFUNCTIONS>> _HOOKEDFUNCTION;
 
+//----------------------------------------------------------------------------------------------------------
+LPVOID  MK_VirtualAlloc(VOID* startAddress,SIZE_T size) {
+    ULONG* requiredFunctionAddress = (ULONG*)startAddress;
+    LPVOID address=NULL;
+    while (address == NULL)
+    {
+        address = VirtualAlloc(requiredFunctionAddress, size, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        requiredFunctionAddress = requiredFunctionAddress + 0x1000;
+    }
+    memset(address, 0x90, size);
+    DWORD testValue_1 = 0;
+    if (VirtualProtect(address, size, PAGE_EXECUTE_READWRITE, &testValue_1) == 0) {
+        cout << -1;
+        return 0;
+    }
+    return address;
+
+}
+VOID MK_LongJump(VOID * gotoAddress,VOID * setAddress) {
+
+    unsigned char* uc_shortJumpAddress = (unsigned char*)setAddress;
+    *uc_shortJumpAddress = (unsigned char)0x48;
+    *(uc_shortJumpAddress + 1) = (unsigned char)0xB8;
+    *((long long int*)(uc_shortJumpAddress + 2)) = (long long int)gotoAddress;
+    *((unsigned short*)(uc_shortJumpAddress + 10)) = 0xe0ff;
+}
+//----------------------------------------------------------------------------------------------------------
+int MK_VirtualProtect(VOID* memoryAddress,SIZE_T size) {
+    DWORD testValue_1 = 0;
+    if (VirtualProtect(memoryAddress, size, PAGE_EXECUTE_READWRITE, &testValue_1) == 0) {
+        cout << -2;
+        return -2;
+    }
+    memset(memoryAddress,0x90,size);
+}
+
+//----------------------------------------------------------------------------------------------------------
 typedef int (WINAPI* TB_MessageBoxA)(HWND, LPCSTR, LPCSTR, UINT);
 TB_MessageBoxA TBMessageBoxA = NULL;
-//----------------------------------------------------------------------------------------------------------
-int HOOK_CREATE(VOID* pOriginAddress, VOID* pHookFuncAddress,VOID* tramFuncAddress) {
+int WINAPI TEST(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
+{
+    cout << "TEST VERISI" << endl;
+    return 0;
+}
+int MK_HookCreate(VOID* pOriginAddress, VOID* pHookFuncAddress,VOID* tramFuncAddress) {
+    //LPVOID* ppOriginal = reinterpret_cast<LPVOID*>(tramFuncAddress);
     INFORMATIONABOUTHOOKEDFUNCTIONS newHookInformation = { 0 };
+    ULONG* requiredFunctionAddress = (ULONG*)pOriginAddress;
+
     
-     DWORD testValue_1=0;
-     if (VirtualProtect(pOriginAddress, FIRSTBYTECPY, PAGE_EXECUTE_READWRITE, &testValue_1) == 0) {
-         cout << -1;
-         return -1;
-     }
-     ULONG* requiredFunctionAddress = (ULONG*)pOriginAddress;
-     while (newHookInformation.tramAddress == NULL)
-     {
-         newHookInformation.tramAddress = VirtualAlloc(requiredFunctionAddress, TRAMPOLINBYTE, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-         requiredFunctionAddress = requiredFunctionAddress - 0x1000;
-     }
-     memset(newHookInformation.tramAddress, 0x90, TRAMPOLINBYTE);
-     memcpy(newHookInformation.tramAddress, pOriginAddress, FIRSTBYTECPY);
-     unsigned char* deneme1 = (unsigned char*)newHookInformation.tramAddress;
-     for (int i = 0; i < TRAMPOLINBYTE; i++)
-         cout << hex << (int)deneme1[i] << endl;
-     memset(pOriginAddress, 0x90, FIRSTBYTECPY);
-     *((unsigned char*)pOriginAddress) = (unsigned char)0x48;
-     *((unsigned char*)pOriginAddress + 1) = (unsigned char)0xB8;
-     *((long long int*)((unsigned char*)pOriginAddress + 2)) = (long long int)pHookFuncAddress;
-     *((unsigned short*)((unsigned char*)pOriginAddress + 10)) = 0xe0ff;
+    newHookInformation.shortJumpAddress = MK_VirtualAlloc(pOriginAddress, SHORTJUMPBYTE);
+    newHookInformation.trambolineAddress = MK_VirtualAlloc(pOriginAddress, TRAMPOLINEBYTE);
+    memcpy(newHookInformation.trambolineAddress, pOriginAddress,5);
+    MK_VirtualProtect(pOriginAddress, FIRSTBYTECPY);
+ 
+    unsigned char* deneme9 = (unsigned char*)newHookInformation.trambolineAddress;
+    for (int i = 0; i < 20; i++) {
+        cout << hex << (int)deneme9[i] << endl;
+    }
+    cout << "-------------------------------------" << endl;
+    //---------------------------->>>>>>>>>>>>>>
 
-     cout << "pOriginAddress:               :" << pOriginAddress << endl;
-     cout << "newHookInformation.tramAddress:" << newHookInformation.tramAddress << endl;
+    MK_LongJump((unsigned char*)pOriginAddress+ FIRSTBYTECPY, (unsigned char*)newHookInformation.trambolineAddress + FIRSTBYTECPY);
+    tramFuncAddress = (TB_MessageBoxA)newHookInformation.trambolineAddress;
 
-     ULONG* offset = (ULONG*)newHookInformation.tramAddress - (ULONG)pOriginAddress;
-     
-     cout << "----------------------------------------" << endl;
+    for (int i = 0; i < 20; i++) {
+        cout << hex << (int)deneme9[i] << endl;
+    }
+    //----------------------------<<<<<<<<<<<<<<
 
-     *((unsigned char*)newHookInformation.tramAddress + FIRSTBYTECPY +1) = (unsigned char)0x48;
-     *((unsigned char*)newHookInformation.tramAddress + 1 + FIRSTBYTECPY +1) = (unsigned char)0xB8;
-     *((long long int*)((unsigned char*)newHookInformation.tramAddress + 2 + FIRSTBYTECPY +1)) = (long long int)pOriginAddress + FIRSTBYTECPY+1 ;
-     *((unsigned short*)((unsigned char*)newHookInformation.tramAddress + 10 + FIRSTBYTECPY+1 )) = 0xe0ff;
-
-     tramFuncAddress = (TB_MessageBoxA)newHookInformation.tramAddress;
-    
-     for (int i = 0; i < TRAMPOLINBYTE; i++)
-         cout << hex << (int)deneme1[i] << endl;
-     
-     
-     newHookInformation.nameAddress     = (ULONG)pOriginAddress;
-     newHookInformation.oriAddress      = (ULONG)pOriginAddress;
-     newHookInformation.hookFuncAddress = (ULONG)pHookFuncAddress;
-     newHookInformation.tramFuncAddress = tramFuncAddress;
-     _HOOKEDFUNCTION.push_back(make_pair((ULONG)pOriginAddress, newHookInformation));
+    long long int offset =((long long int)newHookInformation.shortJumpAddress- (long long int)pOriginAddress)-5;
+    memset(pOriginAddress,0xE9, 1);
+    memcpy((unsigned char*)pOriginAddress+1, &offset, 4);
+    MK_LongJump(pHookFuncAddress, newHookInformation.shortJumpAddress);
 
      return 0;
  }
 //----------------------------------------------------------------------------------------------------------
 
-
 int WINAPI HK_MessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
 {
     cout << "BISELER OLMAYA BASLADI" << endl;
     TBMessageBoxA(NULL, "TUGÇEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "asdas", MB_OK);
-    
     return 0;
 }
+
 //----------------------------------------------------------------------------------------------------------
 void clear() {
-    for (int i = 0; i < _HOOKEDFUNCTION.size(); i++) {
-        //free(_HOOKEDFUNCTION.at(i).second.tramFuncAddress);
+    for (int i = 0; i < _HookedFunction.size(); i++) {
+      // VirtualFree(_HookedFunction.at(i).second.trambolineAddress,);
     }
 }
 //----------------------------------------------------------------------------------------------------------
+
 int main() {
-    HOOK_CREATE(&MessageBoxA,&HK_MessageBoxA,&TBMessageBoxA);
+    MK_HookCreate(&MessageBoxA,&HK_MessageBoxA,&TBMessageBoxA);
     MessageBoxA(NULL, "TUGÇE", "asdas", MB_OK);
-    clear();
+//    return 0;
 }
 //----------------------------------------------------------------------------------------------------------
